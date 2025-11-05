87e3bc3f793f3a8b8aa4595daad8f06a8ca957df
Julisa Verdejo <julisa.verdejo@gmail.com>
2025-11-05 11:00:12 +0100

    feat: create project which guess the weekday from an input txt file, returns an ouput.txt with the weekday

---
+++ 06_cmdline_date/src/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <format>
+#include <vector>
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+int main(int argc, char* argv[]) {
+    Date myDate;
+
+    if (argc < 2) {
+        std::cout << "You did not enter a date. Enter one in this format: DD/MM/YYYY" << std::endl;
+        return 1;
+    }
+    
+    std::string filedate;
+    std::string filename = argv[1];
+    std::ifstream InputFile(filename);
+    std::ofstream OutputFile("output.txt");
+
+    if (!InputFile.is_open()) {
+        std::cout << "[ERROR]: Could not open the file " << filename << std::endl;
+        return 1;
+    }
+    
+    std::getline(InputFile, filedate);
+    // int i = 0;
+    // // std::vector<int> date_array;
+
+    // // if (filedate.length() != 10) {
+    // //     std::cout << "You did not enter a valid date" << std::endl;
+    // // } else {
+    // //     while (i < 10) {
+    // //         date_array.push_back()
+
+    // //         i++;
+    // //     }
+    // // }
+
+    myDate.day = std::stoi(filedate.substr(0,2));
+    myDate.month = std::stoi(filedate.substr(3,2));
+    myDate.year = std::stoi(filedate.substr(6,4));
+
+    auto date = std::chrono::year_month_day{std::chrono::year{myDate.year}, std::chrono::month{unsigned(myDate.month)}, std::chrono::day{unsigned(myDate.day)}};
+
+    if (date.ok()) {
+        auto sys_days = std::chrono::sys_days{date};
+        auto weekday = std::chrono::weekday{sys_days};
+        OutputFile << std::format("{:%A}", weekday) << " " << std::format("{:%d %B, %Y}", date) << std::endl;
+    } else {
+        OutputFile << "[ERROR]: " << std::format("{:%d %B, %Y}", date) << " is not a valid date" << std::endl;
+    }
+
+    InputFile.close();
+    OutputFile.close();
+    return 0;
+}
